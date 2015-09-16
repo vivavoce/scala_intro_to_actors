709d63a416bfb16a6902628d7d0708a58b9f1371
Jonathan Zar <vivavoce@pingalo.com>
2015-09-15 23:30:54 -0700

    03 adding src directory and app outline

---
+++ src/main/scala/workingActors.scala
@@ -0,0 +1,18 @@
+package workingActors  // an introduction to Actors, workers, and Scala programming
+
+// NOTICES
+
+
+// CONTEXT
+
+
+// IMPORTS
+
+
+// MESSAGES defined within this file's namespace
+
+
+// CLASSES
+
+
+// START OF APP
